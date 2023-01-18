fca4f6459c0bbd38adbaebea2632bb2d931c282c
priyanka <bharadwajpriyanka204@gmail.com>
2023-01-18 01:42:38 -0800

    calculator

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:calculator/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Cal(),
+  ),
+  );
+}
